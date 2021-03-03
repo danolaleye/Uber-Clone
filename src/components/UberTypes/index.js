6597e268d3b494e32c4972a3411f5e66221b312d
Dan Olaleye <danolaleye@gmail.com>
2021-03-03 01:11:24 -0500

    Ride order submission

--- src/components/UberTypes/index.js
+++ src/components/UberTypes/index.js
@@ -1,30 +1,37 @@
 import React from "react";
 import { View, Text, Pressable } from "react-native";
-import styles from './styles.js';
-import UberTypeRow from '../UberTypeRow';
+import styles from "./styles.js";
+import UberTypeRow from "../UberTypeRow";
 
-import typesData from '../../assets/data/types';
+import typesData from "../../assets/data/types";
 
-const UberTypes = (props) => {
+const UberTypes = ({ typeState, onSubmit }) => {
+  const [selectedType, setSelectedType] = typeState;
   const confirm = () => {
-    console.warn('confirm');
+    console.warn("confirm");
   };
 
   return (
     <View>
       {typesData.map((type) => (
-        <UberTypeRow type={type} key={type.id} />
+        <UberTypeRow
+          type={type}
+          key={type.id}
+          isSelected={type.type === selectedType}
+          onPress={() => setSelectedType(type.type)}
+        />
       ))}
 
-      <Pressable onPress={confirm} style={{
-        backgroundColor: 'black',
-        padding: 10,
-        margin: 10,
-        alignItems: 'center',
-      }}>
-        <Text style={{color: 'white', fontWeight: 'bold'}}>
-          Confirm Uber
-        </Text>
+      <Pressable
+        onPress={onSubmit}
+        style={{
+          backgroundColor: "black",
+          padding: 10,
+          margin: 10,
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "white", fontWeight: "bold" }}>Confirm Uber</Text>
       </Pressable>
     </View>
   );
--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -1,16 +1,65 @@
-import React from "react";
-import { View, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { View, Dimensions, Alert } from "react-native";
+import { API, graphqlOperation, Auth } from "aws-amplify";
 import RouteMap from "../../components/RouteMap";
 import UberTypes from "../../components/UberTypes";
+import { createOrder } from "../../graphql/mutations";
 
-import { useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 
 const SearchResults = (props) => {
+  const typeState = useState(null);
   const route = useRoute();
+  const navigation = useNavigation();
 
   // console.log(route.params.originPlace.details.geometry.location.lat);
   const { originPlace, destinationPlace } = route.params;
-  console.log(destinationPlace);
+  // console.log(destinationPlace);
+
+  const onSubmit = async () => {
+    const [type] = typeState;
+    if (!type) {
+      return;
+    }
+
+    try {
+      const userInfo = await Auth.currentAuthenticatedUser();
+
+      const date = new Date();
+      const input = {
+        createdAt: date.toISOString(),
+        type,
+        originLatitude: originPlace.details.geometry.location.lat,
+        originLongitude: originPlace.details.geometry.location.lng,
+
+        destLatitude: destinationPlace.details.geometry.location.lat,
+        destLongitude: destinationPlace.details.geometry.location.lng,
+
+        userId: userInfo.attributes.sub,
+        carId: "1",
+      };
+
+      const response = await API.graphql(
+        graphqlOperation(createOrder, {
+          input,
+        })
+      );
+
+      console.log("iyawon -----> " + JSON.stringify(response));
+      Alert.alert(
+        "Order Success",
+        "Your request has been placed. Your ride is 2 minutes away",
+        [
+          {
+            text: "Ok",
+            onPress: () => navigation.navigate("Home"),
+          },
+        ]
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <View style={{ display: "flex", justifyContent: "space-between" }}>
@@ -19,7 +68,7 @@ const SearchResults = (props) => {
       </View>
 
       <View style={{ height: 400 }}>
-        <UberTypes />
+        <UberTypes typeState={typeState} onSubmit={onSubmit} />
       </View>
     </View>
   );
